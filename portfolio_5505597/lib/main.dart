cb431d23c4f1712d30e4ccd29bd2f9658d28cf2b
J.R <73106528+JRen2002@users.noreply.github.com>
2024-06-14 09:49:19 +0000

    Naivgation hinzugefügt

--- portfolio_5505597/lib/main.dart
+++ portfolio_5505597/lib/main.dart
@@ -1,125 +1,206 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mein Portfolio',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
+      routes: {
+        '/home': (context) => HomePage(),
+        '/about': (context) => AboutPage(),
+        '/resume': (context) => ResumePage(),
+        '/skills': (context) => SkillsPage(),
+        '/projects': (context) => ProjectsPage(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Startseite'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            DrawerHeader(
+              child: Text('Navigation'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ListTile(
+              title: Text('Startseite'),
+              onTap: () {
+                Navigator.pushNamed(context, '/home');
+              },
+            ),
+            ListTile(
+              title: Text('Über mich'),
+              onTap: () {
+                Navigator.pushNamed(context, '/about');
+              },
+            ),
+            ListTile(
+              title: Text('Lebenslauf'),
+              onTap: () {
+                Navigator.pushNamed(context, '/resume');
+              },
+            ),
+            ListTile(
+              title: Text('Fähigkeiten'),
+              onTap: () {
+                Navigator.pushNamed(context, '/skills');
+              },
+            ),
+            ListTile(
+              title: Text('Projekte'),
+              onTap: () {
+                Navigator.pushNamed(context, '/projects');
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Center(
+        child: Text('Willkommen auf meiner Startseite!'),
+      ),
+    );
+  }
+}
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Über mich'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Card(
+              child: ListTile(
+                title: Text('Persönliche Informationen'),
+                subtitle: Text('Mein Name ist Jonas Renner und bin 22 Jahre alt'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('Interessen'),
+                subtitle: Text('Fußball'),
+              ),
+            ),
+            // Weitere Cards für andere Informationen hinzufügen
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ResumePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lebenslauf'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Card(
+              child: ListTile(
+                title: Text('Ausbildung'),
+                subtitle: Text('Abitur 2023 auf der Johann-Philipp-Reis-Schule'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('Berufliche Erfahrungen'),
+                subtitle: Text('Seit 2020 Aushilfe in einer Arztpraxis'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('Wichtige Projekte'),
+                subtitle: Text('Keine Angabe'),
+              ),
+            ),
+            // Weitere Cards für andere Abschnitte hinzufügen
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SkillsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fähigkeiten'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Card(
+              child: ListTile(
+                title: Text('Programmiersprachen'),
+                subtitle: Text('C#, Java'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('Tools'),
+                subtitle: Text('Microsoft 365, Adobe Photoshop'),
+              ),
+            ),
+            // Weitere Cards für andere Fähigkeiten hinzufügen
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProjectsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Projekte'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Card(
+              child: ListTile(
+                title: Text('Flappybird'),
+                subtitle: Text('Ein einfaches Flappybird Spiel mit extra Modifizierungen in Windows Forms mit C#'),
+              ),
+            ),
+            // Weitere Cards für andere Projekte hinzufügen
+          ],
+        ),
+      ),
     );
   }
 }
